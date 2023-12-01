d3e68e9099381c0db68ea74e3d5607f02f150365
z.terehov <zaharkuli67@gmail.com>
2023-12-01 11:41:45 +0300

    Added rotation

--- PlanetsAR/Assets/Scripts/RatationController.cs
+++ PlanetsAR/Assets/Scripts/RatationController.cs
@@ -1,18 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
+using UnityEngine.WSA;
 
 public class RatationController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject Planet;
+    public Vector3 Rotation;
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        Planet.transform.Rotate(Rotation * Time.deltaTime);
     }
 }
